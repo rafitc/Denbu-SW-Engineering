3a8eab8740ac2e3022fd3cf0f168eb8f7008e001
Rafi <rafirasheed.tc99@gmail.com>
2022-03-05 18:35:28 +0000

    best fit algo

--- denbu_mem.c
+++ denbu_mem.c
@@ -13,6 +13,25 @@ int callsToInit = 0;
 // size of page in virtual memory
 int pageSize;
 
+//Payload header to notify free or allocated
+// 0 -> Not allocated
+// 1 -> Allocated
+typedef struct header{
+    struct header* next;
+    
+    // 0 -> Not allocated
+    // 1 -> Allocated
+    int alloc_flag;
+    int size;
+    
+}payload_header;
+
+payload_header* head_block = NULL;
+
+int checkFree(payload_header* test_block);
+void* check_best_fit(int mem_size);
+//function to check whether the space is free or not 
+
 int Denbu_Mem_Init(int sizeOfRegion)
 {
     void *memmptr;
@@ -40,6 +59,14 @@ int Denbu_Mem_Init(int sizeOfRegion)
         perror("mmap");
         exit(1);
     }
+    //save the mem space 
+    head_block = (payload_header*)memmptr;
+    printf("Head block space %p\n", head_block);
+    head_block->next = NULL;
+    head_block->alloc_flag = 1;
+    head_block->size = sizeOfRegion - (int)sizeof(payload_header);
+    printf("Allocation status of head block %d\n", head_block->size);
+    
     // this will not free the memory we got from OS.
     close(fd);
     
@@ -54,3 +81,90 @@ int Denbu_Mem_Init(int sizeOfRegion)
 // TODO: Library should fill the rest of the functions
 // Denbu_Mem_Alloc, Denbu_Mem_Free, Denbu_Mem_Dump
 //
+void* Denbu_Mem_Alloc(int mem_size_test, int style){
+
+    while(!(mem_size_test % 8 == 0))
+	{
+		++mem_size_test;
+	}
+    printf("mem size after updation %d\n", mem_size_test);
+
+
+    switch(style){
+        case 1:
+            printf("BESTFIT \n");
+            return(check_best_fit(mem_size_test));
+            break;
+        case 2:
+            printf("FIRSTFIT \n");
+            break;
+        case 3:
+            printf("WORSTFIT \n");
+            break;
+        default:
+            printf("Default case \n");
+            break;
+
+    }
+    return 0;
+}
+
+void* check_best_fit(int mem_size){
+    printf("Best fit fn call\n");
+    int best_fit_block_size = 0; // neet to change change var to hold size (including header) of bf_block
+    payload_header* current_block = head_block; 
+    payload_header* best_fit_block = NULL;
+
+    while(current_block != NULL){
+
+        //chekc is the location free or not
+        if(checkFree(current_block)){
+            printf("IF check free fn\n");
+            if(best_fit_block_size == 0){ //Look for previous allocation
+                if(current_block->size >= mem_size){
+                    best_fit_block_size = current_block->size + 8;
+                    printf("\nhere is the freeBlockSize : %d", best_fit_block_size);
+                    best_fit_block = current_block;
+                }
+            }
+            if((current_block->size + 8) < best_fit_block_size && current_block->size >= mem_size){
+
+                best_fit_block_size = current_block->size + 8;
+                best_fit_block = current_block;
+            }
+        }
+        printf("\n Next block");
+        current_block = current_block->next;
+    }
+
+    if(best_fit_block == NULL)
+	{
+	    m_error = E_NO_SPACE;
+        return NULL;
+	}
+
+    best_fit_block->alloc_flag = 1;
+    head_block = best_fit_block+8;
+    head_block->alloc_flag = 1;
+    return ((void*) best_fit_block + 8);
+}
+
+int checkFree(payload_header* test_block){
+    printf("Alloc flag %d",test_block->alloc_flag);
+    if(test_block == NULL){
+        return 0;
+    }
+    else if(test_block->alloc_flag == 1){
+        return 1;
+    }
+    return 0;  
+}
+
+int Denbu_Mem_Free(void *ptr){
+    printf("Denbu Mem Free Fn \n");
+    return 0;
+}
+
+void Denbu_Mem_Dump(){
+    printf("Denbu Mem Dump \n");
+}
--- main.c
+++ main.c
@@ -7,8 +7,18 @@
 int main(int argc, const char *argv[]){
 	printf("Testing out malloc...\n");
 
-	int size = 65536;
+	int size = 4096;
     assert( Denbu_Mem_Init(size) == 0);
+
+	int* ptr = (int*) Denbu_Mem_Alloc(sizeof(int), 1);
+	printf("\nptr : %p", ptr);
+	// ptr = &size;
+	// printf("\nValue at ptr : %d\n", *ptr);
+
+	int* ptr1 = (int*) Denbu_Mem_Alloc(sizeof(int), 1);
+	printf("\nptr1 : %p", ptr1);
+	
+	
 
 	exit(1); 
 }
